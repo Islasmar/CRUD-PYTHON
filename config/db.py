31475ba0dfdb3a46304ab28e5339d68cb1494c3d
Mariano Fajardo Islas <marianoislasfajardo@gmail.com>
2025-03-20 15:59:31 -0600

    Realizando conexión a BD

--- config/db.py
+++ config/db.py
@@ -2,20 +2,22 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv  # Importa dotenv para cargar las variables de entorno
+from dotenv import load_dotenv
 
-# Carga las variables desde el archivo .env
+# Cargar las variables desde el archivo .env
 load_dotenv()
 
-# Obtiene las credenciales desde el entorno
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_NAME = os.getenv("DB_NAME")
+# Obtener las credenciales desde el entorno
+DB_USER = os.getenv("DB_USER", "default_user")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "default_password")
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PORT = os.getenv("DB_PORT", "3306")
+DB_NAME = os.getenv("DB_NAME", "default_db")
 
-# Construye la URL de conexión
-SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+# Construir la URL de conexión
+SQLALCHEMY_DATABASE_URL = (
+    f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
 
 # Crear el engine
 engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
@@ -26,7 +28,7 @@ SesionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 # Definir el objeto base para los modelos
 Base = declarative_base()
 
-
+# Importar modelos
 from models.materiales import Material
 from models.prestamos import Prestamo
-import models.user as User
+from models.user import User
